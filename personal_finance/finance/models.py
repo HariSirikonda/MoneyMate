cf105aca833f17209a577a1084462f574f3e2143
Hari Sirikonda <harisirikonda22@gmail.com>
2026-09-03 10:41:31 +0530

    Added EMIPayments and loan Models

--- personal_finance/finance/models.py
+++ personal_finance/finance/models.py
@@ -3,6 +3,48 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+class Loan(models.Model):
+
+    LOAN_TYPES = [
+        ("home", "Home"),
+        ("car" , "Car"),
+        ("bike" , "Bike"),
+        ("personal" , "Personal"),
+        ("education" , "Education"),
+        ("credit_card" , "Credit Card EMI"),
+        ("others" , "Others")
+    ]
+
+    STATUS_CHOICES = [
+        ("pending" , "Pending"),
+        ("paid" , "Paid"),
+        ("overdue" , "Overdue")
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="transactions")
+    loan_name = models.CharField(max_length=120)
+    loan_type = models.CharField(max_length=20, choices=LOAN_TYPES)
+    principal_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2, help_text="Anual Interest Rate (%)")
+    tenure_months = models.PositiveIntegerField()
+    emi_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    start_date = models.DateField(default=timezone.localdate)
+    end_date = models.DateField(default=timezone.localdate)
+    outstanding_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+
+    def __str__(self):
+        return f"{self.loan_name} - {self.user}"
+
+class EMIPayment(models.Model):
+    loan = models.ForeignKey(Loan, on_delete=models.CASCADE, related_name="payments")
+    installment_number = models.PositiveIntegerField()
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    principal_component = models.DecimalField(max_digits=12, decimal_places=2)
+    interest_component = models.DecimalField(max_digits=12, decimal_places=2)
+    paid_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("paid", "Paid"), ("overdue", "Overdue")], default="pending")
 
 class Transaction(models.Model):
     INCOME = "income"
